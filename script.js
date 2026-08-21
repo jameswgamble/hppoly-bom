bedecf16eadbe728f87106c16632e7bed5a5cf5b
jameswgamble <jameswilliamgamble@gmail.com>
2026-08-20 22:03:23 -0500

    v10.10: temporary restore of script.js (full v10.10 with button fix + resources coming next)

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-PLACEHOLDER
+const VERSION = "v10.10";
+document.getElementById("app").innerHTML = "<p class='p-4 text-amber-800 bg-amber-50 border border-amber-300 rounded'>Updating site to v10.10… please hard-refresh in a moment.</p>";
