5301600f347f5c097e0b2cdf860ce6af5cfcbab6
jameswgamble <jameswilliamgamble@gmail.com>
2026-08-20 23:06:39 -0500

    v10.20 fix: restore script.js with E60/E70 mount + PoE injector support

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-PLACEHOLDER
+const VERSION = "v10.20";
+// script.js – HP | Poly Configurator – v10.20: E60/E70 mount options + PoE+ injector for both
+// Features: Camera mount (VESA E70 / Ceiling E60), power option (wall/injector) for E60+E70, TAA path, promo, MSRP
+
+document.title = 'Poly Video Conferencing "Bill" of Materials Generator';
